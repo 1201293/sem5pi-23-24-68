2bf1834484f571ab3bd47774d063327eba5001ca
JoaoGondar <1201289@isep.ipp.pt>
2023-10-22 15:12:37 +0100

    US250 and 260, list building connections and update buiding connections, completed

--- MasterDataDevices/src/services/IServices/IBuildingConnectionService.ts
+++ MasterDataDevices/src/services/IServices/IBuildingConnectionService.ts
@@ -1,8 +1,9 @@
 import { Result } from "../../core/logic/Result";
 import IBuildingConnectionDTO from "../../dto/IBuildingConnectionDTO";
+import IBuildingDTO from "../../dto/IBuildingDTO";
 
 export default interface IBuildingConnectionService  {
   createBuildingConnection(buildingConnectionDTO: IBuildingConnectionDTO): Promise<Result<IBuildingConnectionDTO>>;
-  listBuildingConnections(): Promise<Result<Array<IBuildingConnectionDTO>>>;
+  listBuildingConnections(buildingDTO: IBuildingDTO): Promise<Result<Array<IBuildingConnectionDTO>>>;
   updateBuildingConnection(buildingConnectionDTO: IBuildingConnectionDTO): Promise<Result<IBuildingConnectionDTO>>;
 }
