46d8fd670af256ee8496c84edc8af9c1fb1fe327
LFilipe03 <97193160+LFilipe03@users.noreply.github.com>
2023-10-25 09:39:24 +0100

    US 270 done (Added verification 1 elevator per building)

--- MasterDataDevices/src/services/IRepos/IElevatorRepo.ts
+++ MasterDataDevices/src/services/IRepos/IElevatorRepo.ts
@@ -4,5 +4,6 @@ import { ElevatorId } from "../../domain/elevatorId";
 
 export default interface IElevatorRepo extends Repo<Elevator> {
     save(elevator: Elevator): Promise<Elevator>;
-
+    findByElevatorId(elevatorId: ElevatorId | string): Promise<Elevator>;
+    findByBuildingId(buildingId: string): Promise<Array<Elevator>>;
 }
