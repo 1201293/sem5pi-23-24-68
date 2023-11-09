d42f506e1f52ec61a86c56d7c4495a61a5488e38
1201293 <1201293@isep.ipp.pt>
2023-11-09 12:27:49 +0000

    last touches on list floors ui

--- UI/src/app/Modules/material/material.module.ts
+++ UI/src/app/Modules/material/material.module.ts
@@ -7,8 +7,10 @@ import {MatStepperModule} from '@angular/material/stepper';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
+import {MatListModule} from '@angular/material/list';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 
 
@@ -16,10 +18,10 @@ import {MatSelectModule} from '@angular/material/select';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,MatButtonModule,MatToolbarModule,MatMenuModule,MatStepperModule,MatIconModule,MatSnackBarModule,MatFormFieldModule,MatInputModule,MatSelectModule
+    CommonModule,MatButtonModule,MatToolbarModule,MatMenuModule,MatStepperModule,MatIconModule,MatSnackBarModule,MatFormFieldModule,MatInputModule,MatSelectModule,MatListModule,MatProgressSpinnerModule
   ],
   exports: [
-    MatButtonModule,MatToolbarModule,MatMenuModule,MatStepperModule,MatIconModule,MatSnackBarModule,MatFormFieldModule,MatInputModule,MatSelectModule
+    MatButtonModule,MatToolbarModule,MatMenuModule,MatStepperModule,MatIconModule,MatSnackBarModule,MatFormFieldModule,MatInputModule,MatSelectModule,MatListModule,MatProgressSpinnerModule
   ],
 })
 export class MaterialModule { }
